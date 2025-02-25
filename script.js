88fc8ec0183d8b042dbccfbd95d85870cb9d3611
Sweater <dev@sweater.agency>
2025-02-25 11:08:20 +0100

    gsap animation v.1

--- script.js
+++ script.js
@@ -1,26 +1,19 @@
-// // // Wait for DOM to load
 
-// // const galleryWrapper = document.querySelector(".gallery-wrapper");
-//  document.addEventListener("DOMContentLoaded", () => {
-//     gsap.registerPlugin(ScrollTrigger);
+ document.addEventListener("DOMContentLoaded", () => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to( ".gallery-section", { //images
+        yPercent: -50,
+        ease: "none",
+        scrollTrigger: {
+            trigger: ".gallery-section",
+            start: "top top ",
+            end: "+=100%", //takes 100 of a trigger ig 
+            markers: true,
+            scrub: true,
+            pin: ".gallery",
+        },
+     });
+});
 
-//     const pictureHeight = document.querySelector(".picture").clientHeight;
-//     const picture = document.querySelectorAll(".picture");
-//     // const galleryEnd = THE last child' bottom in gallery section 
-//     const galleryEnd = document.querySelector(".gallery-wrapper").clientHeight - picture;
-//     console.log(galleryEnd);
 
-//     gsap.to( ".horse-1", {
-//         scrollTrigger: {
-//             trigger: ".horse-1",
-//             start: "top top ",
-//             end: "bottom top",
-//             markers: true,
-//             scrub: true,
-//             pin: ".gallery-wrapper",
-//         },
-//         y: (index) => - pictureHeight * (index + 1), // Move each picture up sequentially
-//         stagger: -0.2,
-//      });
-//     });
 
